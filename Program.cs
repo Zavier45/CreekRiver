c4fe8f3cea6d9cc5f6cf26b6b3824ccb6c7d9178
Zavier Hopson <zo.hopson94@gmail.com>
2024-05-21 16:23:13 -0500

    added endpoint logic

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using CreekRiver.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http.Json;
+using CreekRiver.Models.DTOs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,13 +18,131 @@ AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 builder.Services.AddNpgsql<CreekRiverDbContext>(builder.Configuration["CreekRiverDbConnectionString"]);
 var app = builder.Build();
 
-
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
+app.MapGet("/api/campsites", (CreekRiverDbContext db) =>
+{
+    return db.Campsites.Select(c => new CampsiteDTO
+    {
+        Id = c.Id,
+        Nickname = c.Nickname,
+        ImageUrl = c.ImageUrl,
+        CampsiteTypeId = c.CampsiteTypeId
+    }).ToList();
+});
 
+app.MapGet("api/campsites/{id}", (CreekRiverDbContext db, int id) =>
+{
+    return db.Campsites
+    .Include(c => c.CampsiteType)
+    .Select(c => new CampsiteDTO
+    {
+        Id = c.Id,
+        Nickname = c.Nickname,
+        CampsiteTypeId = c.CampsiteTypeId,
+        CampsiteType = new CampsiteTypeDTO
+        {
+            Id = c.CampsiteType.Id,
+            CampsiteTypeName = c.CampsiteType.CampsiteTypeName,
+            FeePerNight = c.CampsiteType.FeePerNight,
+            MaxReservationDays = c.CampsiteType.MaxReservationDays
+        }
+    })
+    .Single(c => c.Id == id);
+});
 
+app.MapPost("api/campsites", (CreekRiverDbContext db, Campsite campsite) =>
+{
+    db.Campsites.Add(campsite);
+    db.SaveChanges();
+    return Results.Created($"/api/campsites/{campsite.Id}", campsite);
+});
+
+app.MapDelete("/api/campsite/{id}", (CreekRiverDbContext db, int id) =>
+{
+    Campsite campsite = db.Campsites.SingleOrDefault(campsite => campsite.Id == id);
+    if (campsite == null)
+    {
+        return Results.NotFound();
+    }
+    db.Campsites.Remove(campsite);
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+app.MapPut("/api/campsites/{id}", (CreekRiverDbContext db, int id, Campsite campsite) =>
+{
+    Campsite campsiteToUpdate = db.Campsites.SingleOrDefault(campsite => campsite.Id == id);
+    if (campsiteToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    campsiteToUpdate.Nickname = campsite.Nickname;
+    campsiteToUpdate.CampsiteTypeId = campsite.CampsiteTypeId;
+    campsiteToUpdate.ImageUrl = campsite.ImageUrl;
+
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+app.MapGet("/api/reservations", (CreekRiverDbContext db) =>
+{
+    return db.Reservations
+        .Include(r => r.UserProfile)
+        .Include(r => r.Campsite)
+        .ThenInclude(c => c.CampsiteType)
+        .OrderBy(res => res.CheckinDate)
+        .Select(r => new ReservationDTO
+        {
+            Id = r.Id,
+            CampsiteId = r.CampsiteId,
+            UserProfileId = r.UserProfileId,
+            CheckinDate = r.CheckinDate,
+            CheckoutDate = r.CheckoutDate,
+            UserProfile = new UserProfileDTO
+            {
+                Id = r.UserProfile.Id,
+                FirstName = r.UserProfile.FirstName,
+                LastName = r.UserProfile.LastName,
+                Email = r.UserProfile.Email
+            },
+            Campsite = new CampsiteDTO
+            {
+                Id = r.Campsite.Id,
+                Nickname = r.Campsite.Nickname,
+                ImageUrl = r.Campsite.ImageUrl,
+                CampsiteTypeId = r.Campsite.CampsiteTypeId,
+                CampsiteType = new CampsiteTypeDTO
+                {
+                    Id = r.Campsite.CampsiteType.Id,
+                    CampsiteTypeName = r.Campsite.CampsiteType.CampsiteTypeName,
+                    MaxReservationDays = r.Campsite.CampsiteType.MaxReservationDays,
+                    FeePerNight = r.Campsite.CampsiteType.FeePerNight
+                }
+            }
+        })
+        .ToList();
+});
+
+app.MapPost("/api/reservations", (CreekRiverDbContext db, Reservation newRes) =>
+{
+    try
+    {
+        db.Reservations.Add(newRes);
+        db.SaveChanges();
+        return Results.Created($"/api/reservations/{newRes.Id}", newRes);
+    }
+    catch (DbUpdateException)
+    {
+        return Results.BadRequest();
+    }
+});
 
 
 app.Run();
